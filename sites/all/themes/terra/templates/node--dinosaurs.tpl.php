b02853835e7e212381f281c72ac3185920b69f4e
csctwx <csctwx@msn.com>
2015-05-08 15:15:53 -0400

    Updating dinosaurs node
    Updating dinosaurs node page with fields' value

--- sites/all/themes/terra/templates/node--dinosaurs.tpl.php
+++ sites/all/themes/terra/templates/node--dinosaurs.tpl.php
@@ -1,10 +1,23 @@
 <?php 
+  //Create field variables 
   $fields = myfunctionlib_get_fields($content);  
-  extract($fields);
- 
- // kpr($fields); die();
+  //extract($fields);  
+  $animal_specifications = ['name_means', 
+                            'period', 
+                            'order', 
+                            'suborder',
+                            'diet',
+                            'length',
+                            'weight',
+                            'discoverer',
+                           ]; 
+  $product_informations = ['product_no', 
+                            'age_grade', 
+                            'measurements', 
+                            'piece_count',                            
+                          ];                       
+  // kpr($content); die();
 ?>
-
 <div id="productTitleDetailPageDino">
   <h4>Dinosaurs</h4>
   <h2>Pachyrhinosaurus</h2>
@@ -12,76 +25,54 @@
 <div class="productTitleDetailPageThumbContainer">
   <div class="productTitleDetailPageThumb"><a href="#"><img src="<?php print_theme_path('images/AN4000-pr-b-PACHY.png'); ?>" width="80" height="80" onClick="MM_swapImage('stage','','<?php print_theme_path('images/AN4000-pr-b-PACHY.png'); ?>',1)"/></a></div>
   <div class="productTitleDetailPageThumb"><a href="#"><img src="<?php print_theme_path('images/AN4000-pkg-PACHY.png'); ?>" width="80" height="80" onClick="MM_swapImage('stage','','<?php print_theme_path('images/AN4000-pkg-PACHY.png'); ?>',1)"/></a></div>
-
 </div>
 <!--<div class="productTitleDetailPageThumbTwo"></div>-->
-<div id="productPictureMapContainer" style="background:ivory url(<?php print_theme_path('images/map-northAmericaAI3.jpg'); ?>) no-repeat right; background-size:contain;">
+<div id="productPictureMapContainer" style="background:ivory url(<?php echo $fields['habitat_map']['url']; ?>) no-repeat right; background-size:contain;">
   <div id="productPictureInnerContainer">
     <div id="magnifyingGlassIcon">
       <img src="<?php print_theme_path('images/magnifyingGlassIcon-TEMP.png'); ?>" />
     </div>
     <span>
-      <img src="<?php print_theme_path('images/AN4001-pr-b-CRYOLO2-REV2.png'); ?>" name="stage" id="stage" />
+      <img src="<?php echo $fields['product_picture']['url']; ?>" name="stage" id="stage" />
     </span>
   </div>
 </div>
 <div class="productTitleDetailPageThumbContainerUnder">
   <div class="productTitleDetailPageThumb"><a href="#"><img src="<?php print_theme_path('images/AN4000-pr-b-PACHY.png'); ?>" width="80" height="80" onClick="MM_swapImage('stage','','<?php print_theme_path('images/AN4000-pr-b-PACHY.png'); ?>',1)"/></a></div>
   <div class="productTitleDetailPageThumb"><a href="#"><img src="<?php print_theme_path('images/AN4000-pkg-PACHY.png'); ?>" width="80" height="80" onClick="MM_swapImage('stage','','<?php print_theme_path('images/AN4000-pkg-PACHY.png'); ?>',1)"/></a></div>
-
-
 </div>
 <div id="productDescriptionContainer">
   <!--<div id="productSpecsFactsWrapper">-->
-
-
   <div class="productSpecsContainer">
-    <ul>
-      <li><span class="listSubTitleSpan"><?php echo $name_means['label']; ?>:</span> <?php echo $name_means['value']; ?></li>
-      <li><span class="listSubTitleSpan">Period:</span> Late Cretaceous 73-69 Million Years Ago</li>
-      <li><span class="listSubTitleSpan">Order:</span> Ornithischia</li>
-      <li><span class="listSubTitleSpan">Suborder:</span> Ceratopsia</li>
-      <li><span class="listSubTitleSpan"><?php echo $diet['label'].'-wen'; ?>:</span> <?php echo $diet['value']; ?></li>
-      <li><span class="listSubTitleSpan">Length:</span> 26ft (8m) long</li>
-      <li><span class="listSubTitleSpan">Weight:</span>  4 tons (3.6 metric tons)</li>
-      <li><span class="listSubTitleSpan">Discoverer:</span> Al Lakusta (1972)</li>
-      <li class="habitat"><span class="listSubTitleSpan">Natural Habitat:</span> North America</li>
-
-
-
+    <ul>      
+      <?php foreach ($animal_specifications as $value): ?>
+       <li><span class="listSubTitleSpan"><?php echo $fields[$value]['label']; ?>:</span> <?php echo $fields[$value]['value']; ?></li>  
+      <?php endforeach; ?>
+      <li class="habitat"><span class="listSubTitleSpan"><?php echo $fields['natural_habitat']['label']; ?>--bitat:</span> <?php echo $fields['natural_habitat']['value']; ?></li>
     </ul>
     <div id="productRetailInfoContainer">
-      <ul>
-        <li><span>Product #:</span> AN6001</li>
-        <li><span>Age Grade:</span> 3 years and up</li>
-        <li><span>Measurements:</span> 3" x 4"</li>
-        <li><span>Piece Count:</span> 20 accessories (Terra by Battat&trade; Animals Sold Separately)</li>
+      <ul>        
+        <?php foreach ($product_informations as $value): ?>
+         <li><span><?php echo $fields[$value]['label']; ?>:</span> <?php echo $fields[$value]['value']; ?></li>  
+        <?php endforeach; ?>
         <li><a href="#">Download the Instructions Sheet</a></li>
       </ul>
     </div>
   </div>
-
-
   <div id="productFactsContainer">
     <div class="productHabitatContainer"><span class="listSubTitleSpan">Natural Habitat</span>
       <ol>
         <li><span>North America</span></li>
       </ol>
-
-
-
     </div>
-    <p>Pachyrhinosaurus is a close relative of the more familiar Triceratops. There are three know species of Pachyrhinosaurus. The type specimen Pachyrhinosaurus canadensis was discovered in Alberta, Canada in 1946 by Charles Sternberg and named in 1950. Its range included Alaska and Alberta. Pachyrhinosaurus lakustai was discovered in 1972 by science teacher Al Lakusta. In 2012 a partial skull was found in Alaska and was named Pachyrhinosaurus perotorum after Ross Perot who was a contributor to the museum that sponsored the dig. The big bony boss on the nose distinguishes Pachyrhinosaurus from most ceratopsians.  </p>
-  </div>
+        <?php echo $fields['descriptive_text']['value']; ?>
+    </div>
 </div>
 <div id="crossSellingContainer">
   <div class="crossSellTitleRuler"></div>
   <div class="crossSellTitle">ITEMS FROM THE SAME COLLECTION</div>
   <div class="crossSellTitleRuler"></div>
-
-
   <div class="crossSellingThumbContainer"> 
-
     <div class="crossSellingThumb ImageWrapper BackgroundS ContentWrapperH chrome-fix">
       <a href="http://www.battatco.com">
         <img class="img-responsive" src="<?php print_theme_path('images/AN4000-pr-b-PACHY.png'); ?>" alt="">
