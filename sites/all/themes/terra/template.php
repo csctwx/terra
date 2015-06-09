fa73371b754eebef8567b187590d81282cc28815
csctwx <csctwx@msn.com>
2015-06-09 14:31:57 -0400

    add current page to breadcrumbs

--- sites/all/themes/terra/template.php
+++ sites/all/themes/terra/template.php
@@ -56,4 +56,19 @@ function terra_menu_link(array $variables) {
   return '<li class="' . ($element ['#below'] ? ' has-sub':'') . '">' . $output . $sub_menu . "</li>\n";
 }
 
+function terra_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+  if (!empty($breadcrumb)) {
+    // Adding the title of the current page to the breadcrumb.
+    $breadcrumb[] = drupal_get_title();
+    
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
+
 ?>
