1b13213f2751e0a83ac1bb90ad4c6bab3285bc8b
Wen Xiong Tu <csctwx@msn.com>
2015-06-08 23:37:29 -0400

    update where to buy

---
+++ sites/all/themes/terra/templates/node-00--farm-animals.tpl.php
@@ -0,0 +1,143 @@
+<?php 
+  //Create field variables 
+  $fields = myfunctionlib_get_fields($content);  
+  //extract($fields);  
+  $animal_specifications = array('name_means', 
+                            'period', 
+                            'order', 
+                            'suborder',
+                            'diet',
+                            'length',
+                            'weight',
+                            'discoverer',
+                           ); 
+  $product_informations = array('product_no', 
+                            'age_grade', 
+                            'measurements', 
+                            'piece_count',                            
+                          );                       
+   // kpr($fields); die();
+?>
+<div id="productTitleDetailPageDino">
+  <h4><?php echo str_replace('_', ' ', $type) ; ?></h4>
+  <h2><?php echo $title; ?></h2>
+</div>
+<div class="productTitleDetailPageThumbContainer">
+  <?php foreach ($fields['product_picture']['url'] as $product_picture_url): ?>
+      <?php $picture_url = $product_picture_url['picture_url'];  $thumbnail_url = $product_picture_url['thumbnail_url']; ?>  
+      <div class="productTitleDetailPageThumb"><a href="#"><img src="<?php echo $thumbnail_url; ?>" width="80" height="80" onClick="MM_swapImage('stage','','<?php echo $picture_url; ?>',1)"/></a></div>
+  <?php endforeach; ?>
+</div>
+
+<!--<div class="productTitleDetailPageThumbTwo"></div>-->
+<div id="productPictureMapContainer" style="background:ivory url(<?php echo $fields['habitat_map']['url'][0]['picture_url']; ?>) no-repeat right; background-size:contain;">
+  <div id="productPictureInnerContainer">
+    <div id="magnifyingGlassIcon">
+      <img src="<?php echo myfunctionlib_theme_path('images/magnifyingGlassIcon-TEMP.png'); ?>" />
+    </div>
+    <span>
+      <img src="<?php echo $fields['product_picture']['url'][0]['picture_url']; ?>" name="stage" id="stage" />
+    </span>
+  </div>
+</div>
+<div class="productTitleDetailPageThumbContainerUnder">
+  <?php foreach ($fields['product_picture']['url'] as $product_picture_url): ?>
+      <?php $picture_url = $product_picture_url['picture_url'];  $thumbnail_url = $product_picture_url['thumbnail_url']; ?>  
+      <div class="productTitleDetailPageThumb"><a href="#"><img src="<?php echo $thumbnail_url; ?>" width="80" height="80" onClick="MM_swapImage('stage','','<?php echo $picture_url; ?>',1)"/></a></div>
+  <?php endforeach; ?>
+</div>
+
+<div id="productDescriptionContainer">
+  <!--<div id="productSpecsFactsWrapper">-->
+  <div class="productSpecsContainer">
+    <ul>      
+      <?php foreach ($animal_specifications as $value): ?>
+       <li><span class="listSubTitleSpan"><?php echo $fields[$value]['label']; ?>:</span> <?php echo $fields[$value]['value']; ?></li>  
+      <?php endforeach; ?>
+      <li class="habitat"><span class="listSubTitleSpan"><?php echo $fields['natural_habitat']['label']; ?>:</span> <?php echo $fields['natural_habitat']['value'][0]['value']; ?></li>
+    </ul>
+    <div id="productRetailInfoContainer">
+      <ul>        
+        <?php foreach ($product_informations as $value): ?>
+         <li><span><?php echo $fields[$value]['label']; ?>:</span> <?php echo $fields[$value]['value']; ?></li>  
+        <?php endforeach; ?>
+        <li><a href="#">Download the Instructions Sheet</a></li>
+      </ul>
+    </div>
+  </div>
+  <div id="productFactsContainer">
+    <div class="productHabitatContainer"><span class="listSubTitleSpan">Natural Habitat</span>
+      <?php foreach($fields['natural_habitat']['value'] as $natural_habitat_value): ?>
+        <li><span><?php echo $natural_habitat_value['value']; ?></span></li>
+      <?php endforeach; ?>
+    </div>
+        <?php echo $fields['descriptive_text']['value']; ?>
+    </div>
+</div>
+<div id="crossSellingContainer">
+  <div class="crossSellTitleRuler"></div>
+  <div class="crossSellTitle">ITEMS FROM THE SAME COLLECTION</div>
+  <div class="crossSellTitleRuler"></div>
+  <div class="crossSellingThumbContainer"> 
+    <div class="crossSellingThumb ImageWrapper BackgroundS ContentWrapperH chrome-fix">
+      <a href="http://www.battatco.com">
+        <img class="img-responsive" src="<?php echo myfunctionlib_theme_path('images/AN4000-pr-b-PACHY.png'); ?>" alt="">
+        <div class="PStyleH ContentH TOrange">
+          <div class="Content thumbName">
+            <h2>
+              Pachyrhinosaurus
+            </h2>
+            <p>
+              AN4000
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="crossSellingThumb ImageWrapper BackgroundS ContentWrapperH chrome-fix">
+      <a href="http://www.battatco.com">
+        <img class="img-responsive" src="<?php echo myfunctionlib_theme_path('images/AN4001-pr-CRYOLO.png'); ?>" alt="">
+        <div class="PStyleH ContentH TOrange">
+          <div class="Content thumbName">
+            <h2>
+              Cryolophosaurus
+            </h2>
+            <p>
+              AN4001
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="crossSellingThumb ImageWrapper BackgroundS ContentWrapperH chrome-fix">
+      <a href="http://www.battatco.com">
+        <img class="img-responsive" src="<?php echo myfunctionlib_theme_path('images/AN4002-pr-b-DACENTRURUS.png'); ?>" alt="">
+        <div class="PStyleH ContentH TOrange">
+          <div class="Content thumbName">
+            <h2>
+              Dacentrurus
+            </h2>
+            <p>
+              AN4002
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="crossSellingThumb ImageWrapper BackgroundS ContentWrapperH chrome-fix">
+      <a href="http://www.battatco.com">
+        <img class="img-responsive" src="<?php echo myfunctionlib_theme_path('images/AN4003-pr-NANSHIU.png'); ?>" alt="">
+        <div class="PStyleH ContentH TOrange">
+          <div class="Content thumbName">
+            <h2>
+              Nanshiungosaurusus
+            </h2>
+            <p>
+              AN4003
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
--- sites/all/themes/terra/templates/page--node--11.tpl.php
+++ sites/all/themes/terra/templates/page--node--11.tpl.php
@@ -22,60 +22,72 @@
   <div id="breadcrumb">
     <?php print $breadcrumb; ?>    
   </div>
-  <div id="productTitleDetailPage">    
-    <h2><?php echo $title; ?></h2>
+  <div id="productTitleDetailPage">  
+    <h4><?php echo $title; ?></h4>      
+    <h2>Find TERRA by Battat™<br>Items near you</h2>
   </div>
-  <!-- <div class="thumbPageContainer" style="background:url(images/ivory-transparent-back-TEMP.png) repeat;">-->
-  <div class="thumbPageContainer" style="background:ivory;"> 
-  <div id="about-content">
-    <!-- <h1 class="page-header"><img src="http://www.mybtoys.com/media/images/where/wheretobuy.png" alt="Where to Buy" /></h1> -->
-    <div class="page-text">
-      <p>Specialty stores throughout the United States and Canada carry <strong>B.</strong> toys from time to time. While we can't guarantee these stores will have what you are looking for when you are looking, the following is a list of stores that sometimes carry our toys. It would B. smart to call before you go.</p>
-    </div>
-    <div class="col-xs-12">
+  <table class="thumbPageContainer" >
+
+    <tr>
+      <td colspan="2">Specialty stores throughout the United States and Canada carry <strong>B.</strong> toys from time to time. While we can't guarantee these stores will have what you are looking for when you are looking, the following is a list of stores that sometimes carry our toys. It would B. smart to call before you go.
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2">
       <b>Country:</b>
       <Select class="CountrySelect" id="countrycode" >
         <option value="US">US</option>
         <option value="CA">Canada</option>        
       </Select>
-    </div>
-    <div  class="col-xs-12 col-md-6">
-      <b>Address:&nbsp;</b><input type="text" id="address" style="width:40%" value="">
-    </div>
-    <div  class="col-xs-12 col-md-6">
-      <b>City:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b><input type="text" id="city" style="width:40%" value="">
-    </div>
-    <div  class="col-xs-12">
+      </td>
+    </tr>
+   <tr>
+      <td>
+       <b>Address:&nbsp;</b><input type="text" id="address" style="width:40%" value="">
+      </td>
+
+      <td>
+        <b>City:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b><input type="text" id="city" style="width:40%" value="">
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2">
       <div id="US" class="country" ><b>State, and/or Zipcode:</b></div>            
       <div id="CA" class="country" style="display:none"><b>Province, and/or Postal code:</b></div>
       <input type="text" id="addressInput" style="width:80%" value="">
-    </div>                   
-    
-    <div id="map-container">
+    </td>
+    </tr> 
+    <tr>
+      <td colspan="2">                
       <div id="searchbar">
-        <div id="search-location"> 
-        <div class="col-xs-12">
-          <strong>Radius:</strong>
-          <select name="radiusSelect" id="radiusSelect">
-            <option value="1">1 mile</option>
-            <option value="5">5 miles</option>
-            <option value="10">10 miles</option>
-            <option selected="" value="25">25 miles</option>
-            <option value="50">50 miles</option>
-            <option value="100">100 miles</option>
-          </select>
-        </div>          
-          <br>
-          <br>
-          <input type="button" value="Search Locations" onClick="getcoord();">
-          <br>
-          <div id="map-canvas"></div>
-          <div id="outputDiv"></div>
-        </div>
-        <div id="sidebar"></div>
+          <div id="search-location"> 
+          
+            <strong>Radius:</strong>
+            <select name="radiusSelect" id="radiusSelect">
+              <option value="1">1 mile</option>
+              <option value="5">5 miles</option>
+              <option value="10">10 miles</option>
+              <option selected="" value="25">25 miles</option>
+              <option value="50">50 miles</option>
+              <option value="100">100 miles</option>
+            </select>
+                  
+            <br>
+            <br>
+            <input type="button" value="Search Locations" onClick="getcoord();">
+            <br>
+            
+            <div id="outputDiv"></div>
+          </div>
+          
       </div>
-    </div>
-  </div>
-  </div>
+    </td>
+    </tr> 
+  </table>
+  <div id="map-container">
+    <div id="sidebar"></div>
+    <div id="map-canvas"></div>
+  </div> 
+   
 </div>
 <!-- InstanceEndEditable -->
