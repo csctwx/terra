76e078a934c728fbb539b451529cca3b3ba51f9c
Wen Xiong Tu <csctwx@msn.com>
2015-06-17 23:34:17 -0400

    update footer items list

--- sites/all/themes/terra/templates/node--dinosaurs.tpl.php
+++ sites/all/themes/terra/templates/node--dinosaurs.tpl.php
@@ -1,4 +1,4 @@
-<?php 
+<?php   
   //Create field variables 
   $fields = myfunctionlib_get_fields($content);  
   
@@ -17,9 +17,9 @@
                             'age_grade', 
                             'measurements', 
                             'piece_count',                            
-                          );  
-
- // kpr($fields); die();                       
+                          );
+// kpr($node); kpr($id); die();
+  $other_products = myfunctionlib_get_other_products($type, $node->nid);                  
 ?>
 <div id="productTitleDetailPageDino">
   <h4><?php echo str_replace('_', ' ', $type) ; ?></h4>
@@ -95,24 +95,25 @@
   <div class="crossSellingThumbContainer"> 
 
     <div class="thumbPageContainer row"> 
+        <?php foreach ($other_products as $index => $product):?>  
           <div class="item">
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
               <div class="thumbnail" style="">  
                 <div class="views-field views-field-field-product-picture">        
-                  <div class="field-content"><a href="/terra/dinosaurs">
-                    <img typeof="foaf:Image" src="<?php echo myfunctionlib_theme_path('images/AN4000-pr-b-PACHY.png'); ?>" width="160" height="160" alt=""></a>
+                  <div class="field-content"><a href="<?php echo $product['url']; ?>">
+                    <img typeof="foaf:Image" src="<?php echo $product['thumbnail_url']; ?>" width="160" height="160" alt=""></a>
                   </div>  
                 </div>  
                 <div class="views-field views-field-title">        
                   <span class="field-content">
-                    <a href="/terra/dinosaurs">Pachyrhinosaurus</a>
+                    <a href="/terra/dinosaurs"><?php echo $product['title']; ?></a>
                   </span>  
                 </div>
                </div>
             </div>
           </div>
-
-          <div class="item">
+        <?php endforeach; ?>
+          <!-- <div class="item">
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
               <div class="thumbnail" style="">  
                 <div class="views-field views-field-field-product-picture">        
@@ -163,7 +164,7 @@
                 </div>
                </div>
             </div>
-          </div>
+          </div> -->
     </div>
   </div>
 </div>
